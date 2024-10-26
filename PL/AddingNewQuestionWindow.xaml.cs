b12470a6bd6569879e4d7268b4f3bb6efb6448f3
itzickw <80103682+itzickw@users.noreply.github.com>
2024-10-26 21:57:21 +0300

    more pl windows

---
+++ PL/AddingNewQuestionWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using BL;
+using BL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PL1
+{
+    /// <summary>
+    /// Interaction logic for AddingNewQuestionWindow.xaml
+    /// </summary>
+    public partial class AddingNewQuestionWindow : Window
+    {
+        private BlInterface bl;
+        string Category; 
+        int Level;
+        Question question = null!;
+
+        public AddingNewQuestionWindow(BlInterface bl, string category, int level)
+        {
+            InitializeComponent();
+            this.WindowState = WindowState.Maximized;
+            this.bl = bl;
+            this.Category = category;
+            this.Level = level;
+        }
+
+        private void TypeQuestionSelected(object sender, SelectionChangedEventArgs e)
+        {
+            if (questionType.SelectedItem != null)
+            {
+                if (questionType.SelectedIndex == 1)
+                {
+                    addingButton.Visibility = Visibility.Visible;
+                    optionalAnswersNumberLabel.Visibility = Visibility.Hidden;
+                    optionalAnswersNumber.Visibility = Visibility.Hidden;
+                    optionalAnswersNumberGrid.Visibility = Visibility.Hidden;
+                    question.IsClosedQuestion = false;
+                }
+
+                if (questionType.SelectedIndex == 0)
+                {
+                    addingButton.Visibility = Visibility.Hidden;
+                    optionalAnswersNumberLabel.Visibility = Visibility.Visible;
+                    optionalAnswersNumber.Visibility = Visibility.Visible;
+                    question.IsClosedQuestion = true;
+                }
+            }
+        }
+
+        private void AnswersNumSelected(object sender, SelectionChangedEventArgs e)
+        {
+            if (questionType.SelectedItem != null)
+            {
+                optionalAnswersNumberGrid.Visibility = Visibility.Visible;
+                optionalAnswersNumberGrid.Children.Clear();
+                for (int i = 0; i <= optionalAnswersNumber.SelectedIndex; i++)
+                {
+                    Label label = new Label() { Content = "הכנס תשובה אפשרית " + (i + 1).ToString() };
+                    TextBox textBox = new TextBox();
+                    optionalAnswersNumberGrid.Children.Add(label);
+                    optionalAnswersNumberGrid.Children.Add(textBox);
+                }
+                addingButton.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void AddingQuestionClick(object sender, RoutedEventArgs e)
+        {
+            question = new Question
+            {
+                Category = Category,
+                Level = Level,
+                QuestionContent = questionContent.Text,
+                AnswerContent = answerContent.Text
+            };
+
+            if (question.IsClosedQuestion)
+            {
+                question.OptionalAnswers = new List<Answer>();
+                foreach (var child in optionalAnswersNumberGrid.Children)
+                {
+                    if (child is TextBox textBox)
+                    {
+                        question.OptionalAnswers.Add(new Answer()
+                        {
+                            AnswerContent = textBox.Text
+                        });
+                    }
+                }
+            }
+            bl.AddQuestion(question);
+            Close();
+        }
+    }
+}
---
+++ PL/AdminWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PL1
+{
+    /// <summary>
+    /// Interaction logic for AdminWindow.xaml
+    /// </summary>
+    public partial class AdminWindow : Window
+    {
+        private BlInterface bl;
+        private BL.Entities.User user;
+
+        public AdminWindow(BlInterface bl, BL.Entities.User user)
+        {
+            InitializeComponent();
+            this.WindowState = WindowState.Maximized;
+            this.bl = bl;
+            this.user = user;
+            new Menu(this, mainGrid, bl, user);
+            CreateLevelsColorButtons();
+            CreateCategoriesButtons();
+        }
+
+        private void CreateLevelsColorButtons()
+        {
+            levelsColor.Children.Clear();
+            int level = 0;
+            foreach (string color in bl.GetLevelColorsList())
+            {
+
+                Button button = new Button
+                {
+                    Content = "שלב " + level,
+                    Margin = new Thickness(5),
+                    //Style = Desines.GetRoundButtonStyle(),                    
+                };
+
+                button.Background = (Brush)new BrushConverter().ConvertFromString(color);
+                levelsColor.Children.Add(button);
+                level++;
+            }
+        }
+
+        private void CreateCategoriesButtons()
+        {
+            CategoriesButtons.Children.Clear();
+
+            if (user.UserCategories.Count > 0)
+            {
+                foreach (string c in user.UserCategories)
+                {
+                    Button button = new Button
+                    {
+                        Content = c,
+                        Margin = new Thickness(5),
+                        Style = Desines.GetRoundButtonStyle()
+                    };
+
+                    button.Click += QuestionaryEdit;
+                    CategoriesButtons.Children.Add(button);
+                }
+            }
+
+            else  CategoriesButtons.Children.Add(new System.Windows.Controls.Label() { Content = "עדיין לא יצרת שום קטגוריה" });
+        }
+
+        private void QuestionaryEdit(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            new QuestionaryEditWindow(bl, (string)button.Content, user).Show();
+            Close();
+            return;
+        }
+
+        private void AddCategoryClick(object sender, RoutedEventArgs e)
+        {
+            bl.AddCategory(CategoryName.Text, user.Name);
+            CategoryName.Text = string.Empty;
+            user = bl.GetUser(user.Name);
+            CreateCategoriesButtons();
+        }
+
+        private void DefineLevelClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                BrushConverter converter = new BrushConverter();
+                Brush brush = (Brush)converter.ConvertFromString(LevelColor.Text);
+
+                if (int.TryParse(LevelNumber.Text, out int level) && level <= bl.GetLevelColorsList().Count)
+                {
+                    bl.DefineLevelColor(level, LevelColor.Text);
+                    LevelNumber.Text = string.Empty;
+                    LevelColor.Text = string.Empty;
+                    CreateLevelsColorButtons();
+                }
+                else MessageBox.Show("אנא אכנס מספר שלב חוקי");
+            }
+            catch
+            {
+                MessageBox.Show("אנא אכנס שם צבע חוקי");
+            }
+
+
+        }
+    }
+}
