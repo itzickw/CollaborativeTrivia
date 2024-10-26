95e182ca05fe8159ff70c6b39d61385728f63235
itzickw <80103682+itzickw@users.noreply.github.com>
2024-10-26 21:41:20 +0300

    uploading entities

---
+++ DL/Entities/UserQuestionState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DL.Entities
+{
+    public class UserQuestionState
+    {
+        public string UserName { get; set; }
+        public int QuestionId {  get; set; } 
+    }
+}
