051f3bd771795e8772a8cf3a107d01d31ab64427
itzickw <80103682+itzickw@users.noreply.github.com>
2024-10-26 21:47:16 +0300

    uploading entities

---
+++ BL/Entities/Questionary.cs
@@ -0,0 +1,11 @@
+﻿namespace BL.Entities
+{
+    public class Questionary
+    {
+        public required string Category { get; set; }
+        
+        public int Level { get; set; }
+        
+        public List<Question>? Questions { get; set; }
+    }
+}
---
+++ BL/Entities/State.cs
@@ -0,0 +1,11 @@
+﻿namespace BL.Entities
+{
+    public class State
+    {
+        public required string Category { get; set; }
+        
+        public int Level { get; set; }
+        
+        public List<Question>? AnswerdQuestion { get; set; }
+    }
+}
